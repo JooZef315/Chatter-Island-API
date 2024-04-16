2b6f6d35b0d32e51fe01fa9421c834708ac55807
mohamed youssef <m.youssef315@yahoo.com>
2024-04-16 14:58:13 +0200

    feat: complete all users routes functionality

--- src/services/users/getFriends.ts
+++ src/services/users/getFriends.ts
@@ -1,35 +1,30 @@
-import { eq, or } from "drizzle-orm";
+import { and, eq, ne, or } from "drizzle-orm";
 import { db } from "../../config/DB";
 import { users, friends } from "../../drizzle/schema";
-import { CustomError } from "../../utils/customErrors";
 import { alias } from "drizzle-orm/pg-core";
 
 export const getFriends = async (id: string) => {
   const friendAlias = alias(users, "friendAlias");
-  const user = await db
+  const userFriends = await db
     .select({
-      user: users.username,
-      friends,
-      friendAlias: {
-        user: friendAlias.username,
-      },
+      friend_id: friendAlias.id,
+      friend_username: friendAlias.username,
+      statue: friends.status,
     })
     .from(users)
-    .leftJoin(
+    .innerJoin(
       friends,
       or(eq(users.id, friends.user1), eq(users.id, friends.user2))
     )
     .leftJoin(
       friendAlias,
       or(eq(friends.user1, friendAlias.id), eq(friends.user2, friendAlias.id))
-    );
+    )
+    .where(and(eq(users.id, id), ne(users.username, friendAlias.username)));
 
-  // .where(eq(users.id, id));
-
-  //   console.log(user)
-  if (!user.length) {
-    throw new CustomError("user not found", 404);
+  if (!userFriends.length) {
+    return [];
   }
 
-  return user;
+  return userFriends;
 };
